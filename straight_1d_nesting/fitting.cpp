770b6efea92eb545961dbdcd2e1e941d42277e3d
Gökcan Demiralp <gok.can.13@gmail.com>
2021-07-26 15:09:40 +0300

    fixed bugs with fit() and donatello()
    leftmost objects sometimes clipping through the edge fixed

--- straight_1d_nesting/fitting.cpp
+++ straight_1d_nesting/fitting.cpp
@@ -1,89 +1,194 @@
-//het_gokcandemiralp 22-07-21
-#include "fitting.h"
+//het_gokcandemiralp 26-07-21
+#include "slopeFitting.h"
 
 
-int partition(vector<int>& vec, int start, int end){
-	int pivot = end;
-	int j = start;
-	for (int i = start; i < end; ++i) {
-		if (vec[i] < vec[pivot]) {
-			swap(vec[i], vec[j]);
-			++j;
-		}
-	}
-	swap(vec[j], vec[pivot]);
-	return j;
+void flip_horizontal(quad &q){
+    int temp;
+    q.X = -q.X;
+    temp = q.L1;
+    q.L1 = q.L2;
+    q.L2 = temp;
 }
 
-void quickSort(vector<int>& vec, int start, int end) {
-	if (start < end) {
-		int p = partition(vec, start, end);
+void flip_vertical(quad& q) {
+    q.X = q.L1 - q.L2 - q.X;
+}
+
+void rotate(quad a, quad &b) {
+    int D1 = a.L1 - (a.L2 + a.X);
+    int D2 = -b.X;
+    int D3 = b.L1 - b.X - b.L2;
+    
+    if (abs(abs(D1) - abs(D2)) < abs(abs(D1) - abs(D3))) {
+        if (abs(D1) + abs(D2) == abs(D1-D2)) {
+            flip_horizontal(b);
+        }
+    }
+    else {
+        if (abs(D1) + abs(D3) == abs(D1 + D3)) {
+            flip_vertical(b);
+            flip_horizontal(b);
+        }
+        else{
+            flip_vertical(b);
+        }
+    }
+}
+
+
+int offset(quad a, quad b, int gap) {
+    int Offset = 0;
+    int D1 = a.L1 - (a.L2 + a.X);
+    int D2 = -b.X;
+    (D1 - D2 > 0) ? Offset = a.L1 + gap : Offset = a.X + a.L2 - b.X + gap;
+
+    return Offset;
+}
+
+int rightMost(quad a, quad b, int gap) {
+    int Offset = 0;
+    int rightMost = 0;
+    int D1 = a.L1 - (a.L2 + a.X);
+    int D2 = -b.X;
+    (D1 - D2 > 0) ? Offset = a.L1 + gap : Offset = a.X + a.L2 - b.X + gap;
+    (b.L1 > b.X + b.L2) ? rightMost = Offset + b.L1 : rightMost = Offset + b.X + b.L2;
+    (a.L1 > a.X + a.L2) ? rightMost -= a.L1 : rightMost -= (a.X + a.L2);
+
+    return rightMost;
+}
+
+int partition(vector<quad>& vec, int start, int end) {
+    int pivot = end;
+    int j = start;
+    for (int i = start; i < end; ++i) {
+        if ((vec[i].L1 + vec[i].L2) < (vec[pivot].L1 + vec[pivot].L2)) {
+            swap(vec[i], vec[j]);
+            ++j;
+        }
+    }
+    swap(vec[j], vec[pivot]);
+    return j;
+}
+
+void quickSort(vector<quad>& vec, int start, int end) {
+    if (start < end) {
+        int p = partition(vec, start, end);
         quickSort(vec, start, p - 1);
         quickSort(vec, p + 1, end);
-	}
+    }
 }
 
-int binarySearch(vector<int> &vec, int l, int r, int x){
+int binarySearch(vector<quad>& vec, int l, int r, int x) {
     if (r >= l) {
         int mid = l + (r - l) / 2;
-        if (vec[mid] == x)
+        if (2*x == vec[mid].L1 + vec[mid].L2)
             return mid;
         else if (mid == 0)
             return -1;
-        else if (x < vec[mid])
+        else if (2 * x < vec[mid].L1 + vec[mid].L2)
             return binarySearch(vec, l, mid - 1, x);
         else
-            return binarySearch(vec, mid + 1, r, x);       
+            return binarySearch(vec, mid + 1, r, x);
     }
     return r;
 }
 
-int nextSmallest(vector<int>& vec, int x) {
-    return binarySearch(vec, 0, vec.size()-1, x);
+int nextSmallest(vector<quad>& vec, quad a , int total) {
+    int ans;
+    quad b;
+    ans = binarySearch(vec, 0, vec.size() - 1, total);
+
+    while (ans >= 0) {
+        b = vec[ans];
+        rotate(a, b);
+        if ((total - rightMost(a, vec[ans], 0)) > 0) { break; }
+        --ans;
+    }
+
+    return ans;
 }
 
-vector<vector<int>> fit(vector<int> vec , int chunk , int gap) {
-    vector<vector<int>> ans;
+vector<quad>::iterator closestSlope(vector<quad>& vec, quad a) {
+    int D1 = a.L1 - (a.L2 + a.X);
+    int D2 , temp;
+    int lowest = INT_MAX;
+    vector<quad>::iterator ans;
+    quad b;
+
+    for (vector<quad>::iterator e = (vec.end() - 1); vec.size()>0 ;--e) {
+        quad b = (*e);
+        D2 = -b.X;
+        temp = abs(abs(D1) - abs(D2));
+        if (temp == 0) { lowest = temp; return e; }
+        if (temp < lowest) { lowest = temp; ans = e; }
+        if (e == vec.begin()) { break; }
+    }
+    
+    return ans;
+}
+
+vector<vector<quad>> fit(vector<quad> vec, int chunk, int gap) {
     quickSort(vec, 0, vec.size() - 1);
     
+    vector<vector<quad>> ans;
+    int total = 0;
+    quad a = {0,0,0};
+    quad b;
+    vector<quad>::iterator e;
+
     for (int size = vec.size(); size > 0; size = vec.size()) {
-        vector<int> temp;
-        int total = 0;
-        auto e = (vec.end()-1);
+        vector<quad> temp;
+        total = 0;
+        a = { 0,0,0 };
+        e = closestSlope(vec, a);
+        b = (*e);
+        rotate(a, b);
 
-        // It stacks the integers until it doesnt fit anymore starting from the greatest
-        for (; size > 0 && (total + *e) <= chunk; size = vec.size(), e = (vec.end() - 1)) {
-            total = total+*e + gap;
-            temp.push_back(*e);
-            vec.pop_back();
+        for (; size > 0 && (total + rightMost(a, b, gap)) <= chunk; size = vec.size()) {
+            total += rightMost(a, b, gap);
+            temp.push_back(b);
+            vec.erase(e);
             if (vec.end() == vec.begin()) { break; }
+            a = b;
+            e = closestSlope(vec, a);
+            b = (*e);
+            rotate(a, b);
         }
 
-        // It searches for a possible fit for the leftover distance using nextSmallest(), the closest fit possible
-        for (int index = nextSmallest(vec, (chunk - total)); size > 0 && index != -1  ; size = vec.size(), index = nextSmallest(vec, (chunk - total))) {
-            total = total + vec[index] + gap;
+        for (int index = nextSmallest(vec, a, (chunk - total)); size > 0 && index != -1; size = vec.size(), index = nextSmallest(vec, a, (chunk - total))) {
+            b = vec[index];
+            rotate(a, b);
+            total += rightMost(a, b, gap);
             temp.push_back(vec[index]);
-            vec.erase(vec.begin()+index);
+            vec.erase(vec.begin() + index);
+            a = b;
+            e = closestSlope(vec, a);
         }
-        // If there is still leftover place it repeats the step above
-
         ans.push_back(temp);
     }
     return ans;
 }
 
-void picasso(Mat drawing, vector<vector<int>> vec, int gap , int scale, int thicc) {
-    int x = 5;
-    int y = 5;
-    for (auto i = vec.begin(); i != vec.end(); ++i) {
-        int x = 5;
-        for (auto j = (*i).begin(); j != (*i).end(); ++j) {
-            auto p1 = Point(x, y);
-            auto p2 = Point(x + (*j / scale), y + thicc);
-            rectangle(drawing, p1, p2 , Scalar(155, 155, 155), -1);
-           
-            x += (*j + gap ) / scale; //since it is not always an integer the gaps could differ
+void donatello(Mat drawing, vector<vector<quad>> vec, int gap, int thickness) {
+    int Offset = 5;
+    auto i = vec.begin();
+    int Y = 5;
+    quad A = {0,0,0};
+    
+    for (; i != vec.end(); ++i) {
+        auto j = (*i).begin();
+        if (j != (*i).end() && (*j).X < 0) { Offset -= (*j).X;}
+        for (; j != (*i).end(); ++j) {
+            quad a = (*j);
+            vector<Point> poligon = { Point(0 + Offset,Y),Point(a.X + Offset, thickness + Y),Point(a.X + (*j).L2 + Offset,thickness + Y),Point(a.L1 + Offset, Y) };
+            fillConvexPoly(drawing, poligon, cv::Scalar(255, 255, 255));
+
+            if ((j + 1) != (*i).end()) {
+                quad b = *(j + 1);
+                Offset += offset(a, b, gap);
+            }
         }
-        y += 2 * thicc + 1;
-    } 
+        Y += 2 * thickness;
+        Offset = 5;
+    }
 }
